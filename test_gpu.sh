975936bac1a1bdca6342c161c8f9614bf45ae86e
lidongjun2004 <litongjun20040927@outlook.com>
2025-09-21 16:36:50 +0800

    Initial commit

---
+++ test_gpu.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# 训练集
+trainset="mvtec"
+
+# 测试集
+testset="visa"
+
+# 配置名称
+config_name="ViT-L-14-336"
+
+# 输出目录
+output_dir="./output/test_${testset}_${config_name}/"
+
+# 模型文件路径模板
+model_template="./output/train_vpt_${trainset}_${config_name}/model-%s.pth.tar"
+
+model_names=()
+
+# 初始值
+start=3
+end=30
+step=3
+
+# 循环生成数组元素
+for ((i=start; i<=end; i+=step)); do
+    model_names+=("ep$i")
+done
+model_names+=("latest")
+
+# 循环遍历模型文件名数组
+for model_name in "${model_names[@]}"; do
+  # 生成完整的模型文件路径
+  model=$(printf "$model_template" "$model_name")
+  # echo "$model"
+  # 执行测试命令
+  CUDA_VISIBLE_DEVICES=2 python main.py \
+    --config-file ./configs/${config_name}.yaml \
+    --resume "$model" \
+    --output-dir "${output_dir}/${model_name}/" \
+    --name "$testset" \
+    --eval \
+    --device 0 \
+    --vis \
+    --pixel
+done
---
+++ train_gpu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 定义固定的数据集和标志
+dataset="mvtec"
+
+# 定义配置名称
+config_name="ViT-L-14-336"  
+
+# 定义输出目录
+output_dir="./output/train_vpt_${dataset}_${config_name}/"
+
+# 执行训练命令
+CUDA_VISIBLE_DEVICES=2 python main.py \
+  --config-file "./configs/${config_name}.yaml" \
+  --output-dir "$output_dir" \
+  --name "$dataset" \
+  --seed 1003 \
+  --device 0 \
+  --pixel
+
+# --pixel
